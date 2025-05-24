d2fd5737b39a20611a0eac39be4f03298711512c
小 小w <116473366+Xiaomichael@users.noreply.github.com>
2025-05-24 16:04:00 +0800

    水个commit(╹ڡ╹ )

--- RD.py
+++ RD.py
@@ -1,4 +1,4 @@
 def readme():
-  print("this is a project which can release OnePlus Kernel with Root")
+  print("this is a project which can release OnePlus Kernel with KernelSU")
 readme()
 yaap = 'repo init -u https://github.com/RealJohnGalt/opsm8650_kernel_manifest.git -b refs/heads/kplatform -m default.xml --depth=1'
