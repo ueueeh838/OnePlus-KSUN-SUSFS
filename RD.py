f98b133e8130cdab69eda16b2157d14ce84ee21d
小 小w <116473366+Xiaomichael@users.noreply.github.com>
2025-06-21 23:40:30 +0800

    Update RD.py

--- RD.py
+++ RD.py
@@ -2,3 +2,4 @@ def readme():
   print("this is a project which can release OnePlus Kernel with KernelSU")
 readme()
 yaap = 'repo init -u https://github.com/RealJohnGalt/opsm8650_kernel_manifest.git -b refs/heads/kplatform -m default.xml --depth=1'
+blacklist = 'yuri0307'
